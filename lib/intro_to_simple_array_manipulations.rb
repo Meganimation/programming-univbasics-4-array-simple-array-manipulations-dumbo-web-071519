1fdfd746e20a02f10e8b8b11eeaca7b7bae04b4d
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-01 07:26:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,6 +7,42 @@ def using_unshift(array, string)
   array.unshift(string)
 end
 
-def using_pop
-  
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_unshift(array)
+  array.shift 
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array1, array2)
+  array.concat 
+end 
+
+def using_insert(array, element)
+  array.insert(element[3])
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, int)
 end
